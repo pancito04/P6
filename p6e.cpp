66e21da70f9e310aac3a2702902a05f1d6652f18
pancito04 <130194101+pancito04@users.noreply.github.com>
2023-06-21 20:56:15 -0500

    Update p6e.cpp

--- p6e.cpp
+++ p6e.cpp
@@ -11,6 +11,3 @@ class Grupo{
         vector<Alumno>alumnos;
         int cantidad;
 };
-/*A veces cuando creamos programas complejos pueden surgir errores, al realizar operaciones no permitidas, cuando sucede eso el programa se detiene abruptamente y nos suelta un error.
-En c++ hay formas de que nosotros creemos una forma en la cual obligamos al programa a continuar con la ejecución arrastrando el error. 
-*/
